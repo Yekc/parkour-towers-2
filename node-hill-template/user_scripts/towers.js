befbbe1e4c7bd2aa1449da34d9496215873b0f66
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-27 13:49:58 -0500

    sfd

--- node-hill-template/user_scripts/towers.js
+++ node-hill-template/user_scripts/towers.js
@@ -1,4 +1,13 @@
 Game.on("playerJoin", (player) => {
+    //Join messages
     player.message("\\c5Welcome to \\c7Parkour Towers 2\\c5!")
-    player.message("\\c0Towers are ranked from: [#4fe647]Easy\\c0, [#fff94d]Intermediate\\c0, [#ffa747]Hard\\c0, [#f00d00]Very Hard\\c0, [#ff3bfc]Challenging\\c0, to [#4a4a4a]Insane\\c0.")
+    player.message("Towers are ranked from: [#4fe647]Easy\\c0, [#fff94d]Intermediate\\c0, [#ffa747]Hard\\c0, [#f00d00]Very Hard\\c0, [#ff3bfc]Challenging\\c0, to [#4a4a4a]Insane\\c0.")
+    player.message("Good luck!")
+})
+
+//Pain bricks
+world.bricks.filter(brick => brick.name?.startsWith("hurt")).forEach(brick => { 
+    brick.touching(debouncePlayer((player) => {
+        player.health -= 10
+    }), 100)
 })
