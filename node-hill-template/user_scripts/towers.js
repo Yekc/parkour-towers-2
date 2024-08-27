1eed2bc753239184ab92cd20f4096af42a0fe2c9
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-27 13:44:43 -0500

    sdfsdf

--- node-hill-template/user_scripts/towers.js
+++ node-hill-template/user_scripts/towers.js
@@ -1,4 +1,4 @@
 Game.on("playerJoin", (player) => {
     player.message("\\c5Welcome to \\c7Parkour Towers 2\\c5!")
-    player.message("\\c1Towers are ranked from: [#2D8029]Easy\\c1, \\c8Intermediate\\c1, \\c6Hard\\c1, \\c6Very Hard\\c1, \\c9Challenging\\c1, \\c3Insane")
+    player.message("\\c0Towers are ranked from: [#3fb339]Easy\\c0, [#e0db43]Intermediate\\c0, [#eb993f]Hard\\c0, [#cc0b00]Very Hard\\c0, [#de33db]Challenging\\c0, to [#4a4a4a]Insane\\c0.")
 })
