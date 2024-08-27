dd3488d25aaa0e8561035ea1e48302d773986126
Yekc <71790420+Yekc@users.noreply.github.com>
2024-08-27 13:58:54 -0500

    Update main.js

--- node-hill-template/user_scripts/main.js
+++ node-hill-template/user_scripts/main.js
@@ -1,5 +1,5 @@
+//Join messages
 Game.on("playerJoin", (player) => {
-    //Join messages
     player.message("\\c5Welcome to \\c7Parkour Towers 2\\c5!")
     player.message("Towers are ranked from: [#4fe647]Easy\\c0, [#fff94d]Intermediate\\c0, [#ffa747]Hard\\c0, [#f00d00]Very Hard\\c0, [#ff3bfc]Challenging\\c0, to [#4a4a4a]Insane\\c0.")
     player.message("Good luck!")
@@ -13,8 +13,9 @@ world.bricks.filter(brick => brick.name?.startsWith("hurt")).forEach(brick => {
 })
 
 //Teleporters
-world.bricks.filter(brick => brick.name?.startsWith("spawn_tower_")).forEach(brick => { 
+world.bricks.filter(brick => brick.name?.startsWith("portal_")).forEach(brick => { 
     brick.touching(debouncePlayer((player) => {
-        player.health -= 10
-    }), 100)
+        let spawn = world.bricks.find(b => b.name === `tower_${brick.name.split('_')[1]}_spawn`)
+        player.setPosition(new Vector3(spawn.center.x, spawn.center.y, spawn.center.z + 1))
+    }), 1000)
 })
